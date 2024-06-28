8ffd975628da36affa4079fbcb15c280fb3c0f73
Franco <francodanielj@gmail.com>
2024-06-28 16:31:31 -0300

    añadido metodo para traer todos los organismos

--- src/controllers/organismo.controller.ts
+++ src/controllers/organismo.controller.ts
@@ -1,6 +1,42 @@
 import { Request, Response } from "express";
 import { Organismo } from "../entities/organismo";
 
+export const getOrganismos = async (req: Request, res: Response) => {
+  try {
+
+    const organismos = await Organismo.find({
+      relations: {
+        tramite: true,
+      }
+    })
+
+    return res.json(organismos);
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({ message: error.message })
+    }
+  }
+}
+
+//////
+export const getOrganismo = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const organismo = await Organismo.findOne({
+      where: { id: parseInt(id) },
+      relations: ['tramite']
+    });
+
+    if (!organismo) return res.status(404).json({ message: "Organismo no encontrado" })
+
+    return res.json(organismo);
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({ message: error.message });
+    }
+  }
+};
+//////
 export const createOrganismo = async (req: Request, res: Response) => {
   const { name, image_Url } = req.body;
   const organismo = new Organismo();
@@ -11,18 +47,6 @@ export const createOrganismo = async (req: Request, res: Response) => {
   await organismo.save();
   return res.json(organismo);
 };
-//////
-export const getOrganismo = async (req: Request, res: Response) => {
-  try {
-    const organismo = await Organismo.find();
-
-    return res.json(organismo);
-  } catch (error) {
-    if (error instanceof Error) {
-      return res.status(500).json({ message: error.message });
-    }
-  }
-};
 ///////////
 export const updateOrganismo = async (req: Request, res: Response) => {
   const { id } = req.params;
@@ -30,7 +54,7 @@ export const updateOrganismo = async (req: Request, res: Response) => {
     const organismo = await Organismo.findOneBy({ id: parseInt(id) });
 
     if (!organismo) return res.status(404).json({ message: "organismo not found" });
-    
+
     await Organismo.update({ id: parseInt(id) }, req.body);
     return res.sendStatus(204);
   } catch (error) {
@@ -41,14 +65,14 @@ export const updateOrganismo = async (req: Request, res: Response) => {
 };
 ////////////////
 export const deleteOrganismo = async (req: Request, res: Response) => {
-  
+
   const { id } = req.params;
 
   try {
     const result = await Organismo.delete({ id: parseInt(id) });
-    
+
     if (result.affected === 0) return res.status(404).json({ message: "Organismo not found" });
-    
+
     return res.sendStatus(204);
   } catch (error) {
     if (error instanceof Error) {

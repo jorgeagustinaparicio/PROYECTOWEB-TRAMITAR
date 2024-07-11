653468f7bc2769086e86a3f241763ab11a1e27c9
Franco Vilca <75865914+fra92nk@users.noreply.github.com>
2024-07-11 18:53:37 -0300

    "Update database connection settings: changed password and database name."

--- src/db.ts
+++ src/db.ts
@@ -7,8 +7,8 @@ export const AppDataSource = new DataSource({
   host: "localhost",
   port: 3306,
   username: "root",
-  password: "root",
-  database: "pysw",
+  password: "",
+  database: "tramitar",
   // logging: true, // muestra peticiones a la bd
   synchronize: true,
   entities: [Organismo, Tramite],
